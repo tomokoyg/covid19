7510af00b530b4c08617117cd32fa94abef0699b
tomoko <yago_tomoko@intec.co.jp>
2020-03-25 12:08:05 +0900

    test commit

--- pages/index.vue
+++ pages/index.vue
@@ -81,7 +81,7 @@ export default Vue.extend({
       Data,
       headerItem: {
         icon: 'mdi-chart-timeline-variant',
-        title: this.$t('都内の最新感染動向')
+        title: this.$t('tomoko 都内の最新感染動向')
       },
       newsItems: News.newsItems
     }
